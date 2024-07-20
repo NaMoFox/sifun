3a4caa16818b16d57e8a00944d02701b27b6fa24
sofucox <sofyreinel@gmail.com>
2024-07-20 18:14:27 -0500

    joder https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,7 +21,7 @@ class AppServiceProvider extends ServiceProvider
      public function boot(): void
      {
         if (config('app.env') === 'production') {
-            URL::forceSchema('https');
+            \URL::forceSchema('https');
         }
         
      }
